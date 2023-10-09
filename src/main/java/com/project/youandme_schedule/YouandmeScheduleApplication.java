7389c743d398345cd272b7e6e84a27621a791870
Kanggyeonggu <kku1031@naver.com>
2023-10-09 19:34:42 +0900

    init

---
+++ src/main/java/com/project/youandme_schedule/YouandmeScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.project.youandme_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouandmeScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouandmeScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/youandme_schedule/YouandmeScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.youandme_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouandmeScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
